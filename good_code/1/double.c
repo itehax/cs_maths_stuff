0cf4473f046a079dd18dc609161c3fdd41a0a866
itehax <itehaxx@gmail.com>
2025-04-29 20:16:41 +0200

    update

---
+++ good_code/1/double.c
@@ -0,0 +1,70 @@
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int linenum;
+int getword(FILE *fp, char *buf, int size);
+void doubleword(char *fname, FILE *fp);
+
+int main(int argc, char *argv[]) {
+  for (int i = 1; i < argc; ++i) {
+    FILE *fp = fopen(argv[i], "r");
+    if (fp == NULL) {
+      fprintf(stderr, "%s: can't open '%s' (%s)\n", argv[0], argv[i],
+              strerror(errno));
+      return EXIT_FAILURE;
+    } else {
+      doubleword(argv[i], fp);
+      fclose(fp);
+    }
+  }
+  if (argc == 1) {
+    doubleword(NULL, stdin);
+  }
+  return EXIT_SUCCESS;
+}
+
+// reads word from a file, a word is one or more nonspaced chars.
+// getword read the next word from an opened file into buf[0..size-1].
+// return 0 if EOF is reached, otherwise return 1.
+int getword(FILE *fp, char *buf, int size) {
+  int c = getc(fp);
+  // scan forward to a nonspace character or EOF
+  for (; c != EOF && isspace(c); c = getc(fp)) {
+    if (c == '\n')
+      ++linenum;
+  }
+  // copy word into buf[0..size-1]
+  int i = 0;
+  for (; c != EOF && !isspace(c); c = getc(fp)) {
+    if (i < size - 1) {
+      buf[i++] = tolower(c);
+    }
+  }
+  // useless in this case, but always useful.
+  if (i < size) {
+    buf[i] = '\0';
+  }
+  if (c != EOF) {
+    ungetc(c, fp);
+  }
+  return buf[0] != '\0';
+}
+
+void doubleword(char *fname, FILE *fp) {
+  char prev[128], word[128];
+  linenum = 1;
+  prev[0] = '\0';
+
+  while (getword(fp, word, sizeof(word))) {
+    if (isalpha(word[0]) && strcmp(prev, word)) {
+      if (fname) {
+        printf("%s:", fname);
+      }
+      printf("%d: %s\n", linenum, word);
+    }
+    strcpy(prev, word);
+  }
+}
